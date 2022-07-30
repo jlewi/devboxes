4f48e219c516d2413f6a3badb9e5e06ac4c3788d
Jeremy Lewi <jeremy@lewi.us>
2022-07-30 15:47:44 -0700

    List of packages which has conflict.

--- dlbase/packages/conda/install_to_env.sh
+++ dlbase/packages/conda/install_to_env.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 # ENV_NAME is the conda environment, e.g. base
-# PACKAGE_META should be in the form "dlenv-*-meta", which pins dependencies.
 set -x
 
 [[ -n  "${DL_ANACONDA_HOME}" ]] || { echo "DL_ANACONDA_HOME not set" && exit 1; }
-[[ -n  "${CONDA_REPOSITORY}" ]] || { echo "CONDA_REPOSITORY not set" && exit 1; }
 ANACONDA_PYTHON_VERSION="${ANACONDA_PYTHON_VERSION:-3.7}"
 
+DIRECTORY=$(dirname "${BASH_SOURCE[0]}")
 ENV_NAME=$1
-PACKAGE_META=$2
-
-if ! [[ "${PACKAGE_META}" =~ meta ]]; then
-  echo "Package must be in the form dlenv-*-meta, for example dlenv-tf-1-15-cpu-meta."
-  exit 1
-fi
 
 # shellcheck disable=SC1090,SC1091
 . "${DL_ANACONDA_HOME}/etc/profile.d/conda.sh"
 
-echo "Installing ${PACKAGE_META} to environment ${ENV_NAME}."
-# Package name always in the form dlenv-<type>(-version), just get <type>
-[[ "${PACKAGE_META}" =~ dlenv-([a-z]+) ]]
-export PACKAGE_ROOT="${BASH_REMATCH[1]}"
 # shellcheck disable=SC2207
+#
+# TODO(jeremy): This bit of shell magic doesn't appear to work. I believe the intention was to generate
+# a list of channel flags (-c) to be added to the conda install command. Right now we don't use any
+# of the extra channels so it doesn't really matter. We could potentially just hard code them into
+# the command if it becomes a problem.
 CHANNELS=( $(jq -r 'try .[env.PACKAGE_ROOT] | map("-c=\(.)") | .[]' "${BASH_SOURCE%/*}/channels.json") )
 
 # TODO(b/143976717): refactor creation out
@@ -42,20 +36,10 @@ fi
 
 echo "Activating Conda environment: ${ENV_NAME}."
 conda activate "${ENV_NAME}"
-# The base package must be explicitly installed, otherwise when a dependency
-# is changed the package attempts to uninstall itself.
-PACKAGE="${PACKAGE_META%-meta}"
-conda install -y -c "${CONDA_REPOSITORY}" "${CHANNELS[@]}" \
+# Install a bunch of packages listed in python_packages.txt
+# TODO(jeremy): As noted above CHANNELS ends up being empty.
+conda install -y "${CHANNELS[@]}" \
   "python=${ANACONDA_PYTHON_VERSION}" \
-  "${PACKAGE}" \
-  "${PACKAGE_META}" \
-  || { echo "Install of ${PACKAGE_META} failed." && exit 1; }
-# Our metapackages pin exact versions. Remove so user can upgrade/downgrade packages
-echo "Removing metapackage pins."
-conda remove --force "${PACKAGE_META}"
-conda clean -y -a
+  --file=${DIRECTORY}/python_packages.txt \
+  || { echo "Install of ${DIRECTORY}/python_packages.txt failed." && exit 1; }
 
-# Only update permissions on DLVM.
-if [[ -z "${ENV_DOCKER}" ]]; then
-  chmod -R ugo+w "${DL_ANACONDA_HOME}"
-fi
